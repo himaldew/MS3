3eefdb4c519d8eda9c0159c1d5adbcca31e72d76
himaldew <shusamb@yahoo.com>
2020-11-10 23:54:20 +0000

    Add logout function

--- app.py
+++ app.py
@@ -12,15 +12,16 @@ app.config["MONGO_DBNAME"]=os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"]=os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo=PyMongo(app)
+mongo = PyMongo(app)
 
-
+# Get the Recipes from DB
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template ("recipes.html", recipes=recipes)
 
+#Registration functionality
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -31,7 +32,7 @@ def register():
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-
+        #check the passwords 
         if request.form.get("password") == request.form.get("confirmpassword"):
             register = {
                 "username": request.form.get("username").lower(),
@@ -41,10 +42,41 @@ def register():
         # put the new user into 'session' cookie
             session["user"] = request.form.get("username").lower()
             flash("Registration Successful!")
+            return redirect(url_for("login"))
         else:
             flash("Passwords do not match! Please try again")
             return redirect(url_for("register"))
     return render_template("register.html")
+
+#Log in functionality
+@app.route("/login", methods=["GET","POST"]) 
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+
+        if existing_user:
+            #check password hashed 
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("get_recipes", username=session["user"]))
+
+            else:
+                flash("Invalid Username and/or Password")
+                return redirect(url_for("login"))
+                
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")   
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")   
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
