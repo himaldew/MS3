5d0f755981df5c4f498b74f70d4334af739bad7a
himaldew <shusamb@yahoo.com>
2020-11-10 18:00:24 +0000

    Add Confirm password functionality

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
 import os 
-from flask import Flask
+from flask import Flask, flash, render_template, redirect, request, session, url_for 
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"]=os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"]=os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo=PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello world"
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template ("recipes.html", recipes=recipes)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        if request.form.get("password") == request.form.get("confirmpassword"):
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+            mongo.db.users.insert_one(register)
+        # put the new user into 'session' cookie
+            session["user"] = request.form.get("username").lower()
+            flash("Registration Successful!")
+        else:
+            flash("Passwords do not match! Please try again")
+            return redirect(url_for("register"))
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
